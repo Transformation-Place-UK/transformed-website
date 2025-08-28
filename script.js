35807d2ca3672a27a03e9b6210d07cc22023ea16
JereNwa <jnwaokolo@icloud.com>
2025-08-28 14:07:35 +0100

    Fixed navbar links.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('year').textContent = currentYear;
+
+$(document).ready(function() {
+   $(".navbar ul li.disabled a").click(function() {
+     return false;
+   });
+});
