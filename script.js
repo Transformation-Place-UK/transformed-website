6840b3d62e8770f83291eb012864d2b7616c1b32
JereNwa <jnwaokolo@icloud.com>
2025-08-28 14:14:18 +0100

    Fixed typos and deleted unnecessary code.

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('year').textContent = currentYear;
-
-$(document).ready(function() {
-   $(".navbar ul li.disabled a").click(function() {
-     return false;
-   });
-});
